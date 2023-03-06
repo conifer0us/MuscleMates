412c53701c2efadc9146b0f0c1767efb3d89e30d
Connor Ferguson <cnferg04@gmail.com>
2023-03-06 15:17:53 -0500

    Add Endpoint for Updating Profile Information

--- server.js
+++ server.js
@@ -10,22 +10,27 @@ const cookieParser = require('cookie-parser');
 const authlib = require("./libs/Auth.js");
 const proflib = require("./libs/ProfileInfo"); 
 
-// Defines Configuration Options
-const templatepath = __dirname + "/templates";
-const stylepath = __dirname + "/styles";
-const scriptpath = __dirname + "/scripts"
-const port = 80;
-const authdbfile = "auth.db";
+// Loads Configuration Options From config.json
+let configjson = require("./config.json");
+const templatepath = path.join(__dirname, configjson["templatepath"]);
+const stylepath = path.join(__dirname, configjson["stylepath"]);
+const scriptpath = path.join(__dirname, configjson["scriptpath"]);
+const port = configjson["port"];
+const DBFILE = configjson["dbfile"];
 
 // Defines Global Constant Library Objects
 const server = express();
 server.use(cookieParser());
-const auth = new authlib(authdbfile);
-const prof = new proflib(authdbfile);
+const auth = new authlib(DBFILE);
+const prof = new proflib(DBFILE);
 
 // Brings In Externally Defined Routes at Certain Base Folders
-const apiroutes = require("./api.js");
+const apiroutes = require("./routes/api.js");
 server.use("/api", apiroutes);
+
+// Brings in Externally Defined Routes for Posting Information to the Server
+const postroutes = require("./routes/submit.js")
+server.use("/submit", postroutes)
 
 // Send Index File for Homepage Requests
 server.get("/", (req, res) => {
@@ -254,4 +259,3 @@ auth.dbready.then(() => {
         server.listen(port);
     });
 });
-
