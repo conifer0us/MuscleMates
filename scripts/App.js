5fef5e166586ea6e6e357614a9ea640aca6e2363
Antonio Gallego Bernal <ag4258@drexel.edu>
2023-04-17 18:14:57 -0400

    Finished Implementing React, Refactored CSS to Sass

--- scripts/App.js
+++ scripts/App.js
@@ -1,59 +1,6 @@
-const e = React.createElement;
+import { createRoot } from 'react-dom/client';
+import { MyApp } from './Components.js'
 
-function TestMessage() {
-  return (
-    <div>
-      <h1>THIS IS A TESTING PAGE</h1>
-      <p>For regular usage, visit /index.html</p>
-    </div>
-  )
-}
-
-function MyButton() {
-  const [count, setCount] = React.useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-function NavBar() {
-  return (
-    <div className="nav-bar-container">
-      <div className="nav-bar">
-        <div className="logo">
-            <h1><a href="/" className="white-link">GYMDER</a></h1>
-        </div>
-        <div className="links-to-pages">
-            <a href="home" className="nav-bar-item smooth-text">Home</a>
-            <a href="recommendations" className="nav-bar-item smooth-text">Recommendations</a>
-            <a href="friends" className="nav-bar-item smooth-text">Friends</a>
-            <a href="matchrequests" className="nav-bar-item smooth-text">Requests</a>
-            <a href="profile" className="nav-bar-item smooth-text">Profile</a>
-        </div>
-    </div>
-    </div>
-  )
-}
-
-function MyApp() {
-  return (
-    <div>
-      <NavBar />
-      <div className="tab">
-        <TestMessage />
-      </div>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
-
-const app = document.getElementById('app')
-ReactDOM.render(e(MyApp), app)
+const appDomNode = document.getElementById('app')
+const appRoot = createRoot(appDomNode);
+appRoot.render(<MyApp />);
