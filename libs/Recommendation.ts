178b35e97d50f4ab169a04a0ff76bca55ea53b03
Colin McVety <mcvety.colin@gmail.com>
2023-05-05 12:51:57 -0400

    created function calculateScheduleScore and updated function calculateAgeScore in libs/Preferences.ts

--- libs/Recommendation.ts
+++ libs/Recommendation.ts
@@ -1,18 +1,39 @@
 // class for determining what profiles to recommend to a user
-import { ProfileInfo } from './libs/ProfileInfo.ts';
+import { ProfileInfo } from './ProfileInfo';
+import { Preferences } from './Preferences';
 
 export class Recommendation {
     static ageWeight = 1/3
     static scheduleWeight = 1/3
     static workoutWeight = 1/3
 
-    static calculateAgeScore = async (username1 : string, username2: string, ProfileInfo: ProfileInfo) : Promise<> => {
 
-        var profile1Age = ProfileInfo.getAge(username1);
-        var profile2Age = ProfileInfo.getAge(username2);
-        var ageDiff = Math.abs(profile1Age - profile2Age);
-        var ageScore = 1 / (1 + Math.pow(Math.E, -ageDiff)); //sigmoid curve
-        return ageScore
+    //COMMENT HERE
+    static calculateAgeScore = async (username1 : string, username2: string, profileInfo: ProfileInfo) : Promise<Number> => {
+        try {
+            var user1Age = Number(await profileInfo.getAge(username1));
+            var user2Age = Number(await profileInfo.getAge(username2));
+            var ageDiff = Math.abs(user1Age - user2Age);
+            var ageScore = 1 / (1 + Math.pow(Math.E, -ageDiff)); //sigmoid curve
+            return ageScore
+        } catch (error) {
+            if (error) {
+                console.log(error.message)
+                return -1
+            }
+        }
+    }
+
+    static calculateScheduleScore = async (username1 : string, username2 : string, preferences : Preferences) : Promise<Number> => {
+        try {
+            var user1Schedule = await preferences.getSchedule(username1);
+            var user2Schedule = await preferences.getSchedule(username2);
+        } catch (error) {
+            if (error) {
+                console.log(error.message)
+                return -1
+            }
+        }
     }
 
 }
